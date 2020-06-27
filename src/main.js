cf74b41a92a71117875d0ae2cff80bbfdcd8b5d8
André Oliveira <andreprogramador10@gmail.com>
2020-06-27 10:46:08 -0300

    mini-aplicação

--- src/main.js
+++ src/main.js
@@ -1,13 +1,89 @@
-import axios from 'axios';
- const buscaUsuario = async usuario => {
-            try{
-                const response = await axios.get(`https://api.github.com/users/${usuario}`);
-                console.log(response.data);
-            }catch(err){
-                console.log('Usuário não existe');
-            }
-            
-        
+import api from './api';
+
+class App {
+    constructor(){
+        this.repositories = [];
+
+        this.formEl = document.getElementById('repo-form');
+        this.inputEl = document.querySelector('input[name=repository]');
+        this.listEl = document.getElementById('repo-list');
+
+        this.registerHandlers();
+    }
+
+    registerHandlers(){
+        this.formEl.onsubmit = event => this.addRepository(event);
+    }
+
+    setLoading(loading = true){
+        if(loading === true){
+            let loadingEl = document.createElement('span');
+            loadingEl.appendChild(document.createTextNode('Carregando...'));
+            loadingEl.setAttribute('id','loading');
+
+            this.formEl.appendChild(loadingEl);
+        }else{
+            document.getElementById('loading').remove();
+        }
+
+    }
+
+    async addRepository(event){
+        event.preventDefault();
+
+        const repoInput = this.inputEl.value;
+
+        if(repoInput.length === 0){
+            return;
+
+        }
+        this.setLoading();
+        try{
+            const response = await api.get(`/repos/${repoInput}`);
+            const {name, descripition, html_url, owner:{ avatar_url }} = response.data;
+            console.log(response);
+            this.repositories.push({
+                name,
+                descripition,
+                avatar_url,
+                html_url,
+            });
+
+            this.inputEl.value = '';
+            this.render();
+        }catch(err){
+            alert('O repositório não existe!');
+        }
+        this.setLoading(false);
         
     }
-   buscaUsuario('diego3g');
+
+    render(){
+        this.listEl.innerHTML = '';
+
+        this.repositories.forEach(repo =>{
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descripitionEl = document.createElement('p');
+            descripitionEl.appendChild(document.createTextNode(repo.descripition));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target','_blank');
+            linkEl.setAttribute('href', repo.html_url);
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descripitionEl);
+            listItemEl.appendChild(linkEl);
+            
+            this.listEl.appendChild(listItemEl);
+        });
+    }
+}
+new App();
